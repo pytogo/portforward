d3a33026a4ec0f8a4f037d6a5c7ff54b3ce34920
corka149 <corka149@mailbox.org>
2021-07-18 23:44:46 +0200

    Implemented boilerplate and Makefile

--- portforward/portforward.go
+++ portforward/portforward.go
@@ -1,3 +1,14 @@
 package main
 
+import "C"
+import "fmt"
+
+//export PortForward
+func PortForward(namespace *C.char, podName *C.char) {
+	var ns string = C.GoString(namespace)
+	var pod string = C.GoString(podName)
+
+	fmt.Printf("PortForward %s/%s\n", ns, pod)
+}
+
 func main() {}
