7519d6a466867d03b59db5262294f903c5d4312d
corka149 <corka149@mailbox.org>
2021-08-09 22:36:02 +0200

    Fixed type error

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func forward(self *C.PyObject, args *C.PyObject) *C.PyObject {
 
 		msg := fmt.Sprintf("%s", err)
 
-		C.PyErr_SetString(C.PyExc_RuntimeError, msg)
+		C.PyErr_SetString(C.PyExc_RuntimeError, C.PyUnicode_FromString(C.CString(msg)))
 
 		return nil
 	}
