005cf10b4c69a124fb4600f530ec5f93fb35d9dc
corka149 <corka149@mailbox.org>
2021-08-14 15:09:15 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pytogo/portforward",
-    version="0.1.0",
+    version="0.2.0",
     zip_safe=False,
     # Go part
     setup_requires=['setuptools-golang'],
