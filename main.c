1302d5466ccf3bf492e4ef4fad7238218b1c7b46
corka149 <corka149@mailbox.org>
2021-10-11 17:37:57 +0200

    Added note about memory management

--- main.c
+++ main.c
@@ -10,6 +10,13 @@ PyObject* stop(PyObject* , PyObject*);
 To shim go's missing variadic function support.
 
 Ref https://docs.python.org/3/c-api/arg.html
+
+Reminder from docs about memory management when parsing arguments:
+> In general, when a format sets a pointer to a buffer,
+> the buffer is managed by the corresponding Python object,
+> and the buffer shares the lifetime of this object. You
+> won’t have to release any memory yourself. The only
+> exceptions are es, es#, et and et#.
 */
 int PyArg_ParseTuple_ssiis(PyObject* args, char** a, char** b, int* c, int* d, char** e) {
     return PyArg_ParseTuple(args, "ssiis", a, b, c, d, e);
