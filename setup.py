aa071fe4b62c229b49deb75b8f516635616cede0
corka149 <corka149@mailbox.org>
2022-08-23 11:12:18 +0200

    Updated development status

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author_email="corka149@mailbox.org",
     python_requires=">=3.6",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
