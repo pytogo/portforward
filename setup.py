ecda3ef6bcb9701ca25873006d1842669874521f
corka149 <corka149@mailbox.org>
2023-01-26 15:22:54 +0100

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pytogo/portforward",
-    version="0.3.1",
+    version="0.4.0",
     zip_safe=False,
     # Go part
     setup_requires=['setuptools-golang'],
