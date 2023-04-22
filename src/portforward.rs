60e354a3669896dfd3a3580a4432a770feb7c864
corka149 <corka149@mailbox.org>
2023-04-22 19:38:24 +0200

    Use more config; remove logging

--- src/portforward.rs
+++ src/portforward.rs
@@ -31,8 +31,8 @@ static OPEN_PORTFORWARDS: Lazy<RwLock<HashMap<String, Sender<()>>>> = Lazy::new(
 pub struct ForwardConfig {
     namespace: String,
     pod_or_service: String,
-    from_port: u64,
-    to_port: u64,
+    from_port: u16,
+    to_port: u16,
     config_path: String,
     log_level: u64,
     kube_context: String,
@@ -47,34 +47,26 @@ pub async fn forward(config: ForwardConfig) {
 
     let pods: Api<Pod> = Api::default_namespaced(client);
 
-    // Wait until the pod is running, otherwise we get 500 error.
-    info!("waiting for nginx pod to start");
-    let running = await_condition(pods.clone(), "nginx", is_pod_running());
+    let running = await_condition(pods.clone(), &q_name.name, is_pod_running());
     let _ = tokio::time::timeout(std::time::Duration::from_secs(30), running)
         .await
         .unwrap();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    let pod_port = 80;
-    info!(local_addr = %addr, pod_port, "forwarding traffic to the pod");
-    info!(
-        "try opening http://{0} in a browser, or `curl http://{0}`",
-        addr
-    );
-
     let (tx, rx): (Sender<()>, Receiver<()>) = oneshot::channel();
-    register_forward(q_name, tx).await;
+    register_forward(&q_name, tx).await;
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.from_port));
 
     tokio::spawn(async move {
         let server = TcpListenerStream::new(TcpListener::bind(addr).await.unwrap())
             .take_until(rx)
             .try_for_each(|client_conn| async {
-                if let Ok(peer_addr) = client_conn.peer_addr() {
-                    info!(%peer_addr, "new connection");
-                }
                 let pods = pods.clone();
+                let pod_port = config.to_port;
+                let pod_name = q_name.name.clone();
+
                 tokio::spawn(async move {
-                    if let Err(e) = forward_connection(&pods, "nginx", 80, client_conn).await {
+                    if let Err(e) = forward_connection(&pods, &pod_name, pod_port, client_conn).await {
                         error!(
                             error = e.as_ref() as &dyn std::error::Error,
                             "failed to forward connection"
@@ -113,7 +105,6 @@ async fn forward_connection(
     tokio::io::copy_bidirectional(&mut client_conn, &mut upstream_conn).await?;
     drop(upstream_conn);
     forwarder.join().await?;
-    info!("connection closed");
     Ok(())
 }
 
@@ -145,7 +136,7 @@ impl QualifiedName {
     }
 }
 
-async fn register_forward(qualified_name: QualifiedName, sender: Sender<()>) {
+async fn register_forward(qualified_name: &QualifiedName, sender: Sender<()>) {
     OPEN_PORTFORWARDS
         .write()
         .await
